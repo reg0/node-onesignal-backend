ffe1c761172ffa7b56fe5828792fa722a588d1b8
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2018-09-06 08:06:47 +0200

    added /testPush endpoint for sending example push notification

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/health', function (req, res) {
+// respond with "hello world" when a GET request is made to the /health endpoint
+app.get('/health', (req, res) => {
     res.send('hello world');
+});
+
+app.get('/testPush', (req, res) => {
+    require('./pushnotifications.util').sendNotification("Title test", "Text test", ["username1", "username2"]).then(result => {       
+        res.send(result); 
+    });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
